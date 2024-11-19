7360c0fb1845c3a7b981eb32eedd8c0006384a75
matteo190 <matteo.rizzo@sptech.school>
2024-11-19 10:21:11 -0300

    termino da tela inicial e das telas de cadastro e login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -28,7 +28,6 @@ function autenticar(req, res) {
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        cpf: resultadoAutenticar[0].cpf,
                                         aquarios: resultadoAquarios
                                     });
                                 } else {
@@ -57,8 +56,6 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var cpf = req.body.cpfServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -67,14 +64,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (cpf == undefined) {
-        res.status(400).send("Seu cpf está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, cpf, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
---
+++ src/database/tabelas-energiaNuclear.sql
@@ -0,0 +1,9 @@
+create database energiaNuclear;
+use energiaNuclear;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
