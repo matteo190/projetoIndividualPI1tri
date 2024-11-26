ddf1b42e0238ca39fe5633e08038481e5dc06e2b
matteo190 <matteo.rizzo@sptech.school>
2024-11-26 14:18:15 -0300

    deletando diretorios desnecessarios para o projeto

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
 var energiarouter = require("./src/routes/energia")
 
 app.use(express.json());
@@ -29,8 +27,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
 app.use("/energia", energiarouter)
 
 app.listen(PORTA_APP, function () {
